85bf8294432ac96ed9dcc0b0e2358ffd544ab39b
Praveen Yadav <praveen.yadav@ingenico.com>
2022-07-15 18:32:59 +0530

    initial commit

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,24 @@
+package io.ionic.starter;
+
+import com.getcapacitor.BridgeActivity;
+import com.devdactic.contacts.ContactsPlugin;
+import android.os.Bundle;
+ 
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+ 
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      add(ContactsPlugin.class);
+    }});
+  }
+ 
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Plugins } from '@capacitor/core';
+import 'contacts-plugin';
+ 
+const { ContactsPlugin } = Plugins;
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  contacts = [];
+ 
+  constructor() {
+  }
+ 
+  async loadContacts() {    
+    this.contacts = (await ContactsPlugin.getContacts('somefilter')).results;
+    console.log('my contacts: ', this.contacts);    
+  }
 }
